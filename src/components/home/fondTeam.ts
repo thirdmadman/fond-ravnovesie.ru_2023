5b6b4296ebb31d2610dd609278ec5d8bb33fbfec
thirdmadman <thirdmadman@gmail.com>
2023-09-01 15:08:37 +0200

    feat: add fondTeam

---
+++ src/components/home/fondTeam.ts
@@ -0,0 +1,52 @@
+export const fondTeam = {
+  members: [
+    {
+      name: 'Ксения Ковалева',
+      jobTitle: 'Учредитель и директор фонда',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Инна Деникина',
+      jobTitle: 'Попечитель фонда, психолог',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Юлия Ахмедова',
+      jobTitle: 'Попечитель фонда',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Наталия Курбатова',
+      jobTitle: 'Психолог',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Дарья Соловьева',
+      jobTitle: 'Психолог',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Лина Мастерова',
+      jobTitle: 'Координатор проектов',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Мария Кононова',
+      jobTitle: 'Равный консультант, ведущая групп поддержки',
+      description: '',
+      img: ''
+    },
+    {
+      name: 'Мария Копылова',
+      jobTitle: 'Ведущая групп поддержки, фотограф',
+      description: '',
+      img: ''
+    },
+  ],
+}
