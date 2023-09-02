4f8489cd64fc6267a4c0af0e53e83ec3ba937e6a
thirdmadman <thirdmadman@gmail.com>
2023-09-02 16:42:47 +0200

    refactor: remove unused code

--- src/components/home/slideOurTeam.tsx
+++ src/components/home/slideOurTeam.tsx
@@ -6,8 +6,6 @@ import {TeamCarousel} from './teamCarousel';
 import {fondTeam} from './fondTeam';
 
 const OPTIONS: EmblaOptionsType = {};
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export function SlideOurTeam() {
   const leadCardInfo = fondTeam.members[0];
@@ -20,7 +18,7 @@ export function SlideOurTeam() {
       <div className='slide-our-team__list team-list'>
         <div className='team-list__title'>короткая информация по опыту специалистов</div>
         <div className='team-list__cards'>
-          <TeamCard name={leadCardInfo.name} jobTitle={leadCardInfo.jobTitle} imageSrc='../../../public/images/image120.png' isLead />
+          <TeamCard name={leadCardInfo.name} jobTitle={leadCardInfo.jobTitle} imageSrc={leadCardInfo.img} isLead />
           <TeamCarousel
             slides={teamMembers.map((el) => (
               <TeamCard key={el.name} name={el.name} jobTitle={el.jobTitle} imageSrc={el.img} />
