bfc1dc83e386b407ae06023e2d0b4f599d24ec75
thirdmadman <thirdmadman@gmail.com>
2023-09-06 15:38:31 +0200

    refactor: move images

--- src/components/home/fondTeam.ts
+++ src/components/home/fondTeam.ts
@@ -4,43 +4,43 @@ export const fondTeam = {
       name: 'Ксения Ковалева',
       jobTitle: 'Учредитель и директор фонда',
       description: '',
-      img: '/images/image120.png'
+      img: '/images/team/image120.png'
     },
     {
       name: 'Инна Деникина',
       jobTitle: 'Попечитель фонда, психолог',
       description: '',
-      img: '/images/image121.png'
+      img: '/images/team/image121.png'
     },
     {
       name: 'Юлия Ахмедова',
       jobTitle: 'Попечитель фонда',
       description: '',
-      img: '/images/image122.png'
+      img: '/images/team/image122.png'
     },
     {
       name: 'Наталия Курбатова',
       jobTitle: 'Психолог',
       description: '',
-      img: '/images/image92.png'
+      img: '/images/team/image92.png'
     },
     {
       name: 'Дарья Соловьева',
       jobTitle: 'Психолог',
       description: '',
-      img: '/images/image134.png'
+      img: '/images/team/image134.png'
     },
     {
       name: 'Мария Кононова',
       jobTitle: 'Равный консультант, ведущая групп поддержки',
       description: '',
-      img: '/images/image135.png'
+      img: '/images/team/image135.png'
     },
     {
       name: 'Мария Копылова',
       jobTitle: 'Ведущая групп поддержки, фотограф',
       description: '',
-      img: '/images/image136.png'
+      img: '/images/team/image136.png'
     },
   ],
 }
