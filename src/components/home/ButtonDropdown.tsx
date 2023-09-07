874cf0aeb995b8fded89f27e6e842564ac09959e
thirdmadman <thirdmadman@gmail.com>
2023-09-07 23:38:52 +0200

    feat: add button dropdown

---
+++ src/components/home/ButtonDropdown.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+import '../../styles/home/buttonDropdown.scss';
+
+interface ButtonDropdownProps {
+  buttonText: string;
+  children: JSX.Element[];
+}
+// button-dropdown_is-opened
+export function ButtonDropdown({ children, buttonText }: ButtonDropdownProps) {
+  const [isOpened, setIsOpened] = useState(false);
+
+  return (
+    <div
+      className={`button-dropdown ${isOpened ? 'button-dropdown_is-opened' : ''}`}
+      onClick={() => setIsOpened(!isOpened)}
+    >
+      <div className="button-dropdown__text">{buttonText}</div>
+      <ul className="dropdown-list">
+        {children.map((el, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={i}>{el}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
