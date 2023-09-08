7b1526eb84754b268d577790a0f7e3c886fa62b6
thirdmadman <thirdmadman@gmail.com>
2023-09-08 21:00:16 +0200

    feat: add yandex map

--- src/components/contacts/SectionContacts.tsx
+++ src/components/contacts/SectionContacts.tsx
@@ -26,7 +26,33 @@ export function SectionContacts() {
           </a>
         </div>
       </div>
-      <div className="section-contacts__map">!</div>
+      <div className="section-contacts__map">
+        <div style={{ position: 'relative', overflow: 'hidden', width: '100%', height: '100%' }}>
+          <a
+            href="https://yandex.ru/maps/2/saint-petersburg/?utm_medium=mapframe&utm_source=maps"
+            style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}
+          >
+            Санкт‑Петербург
+          </a>
+          <a
+            // eslint-disable-next-line max-len
+            href="https://yandex.ru/maps/2/saint-petersburg/house/dumskaya_ulitsa_1_3/Z0kYdQVoQEICQFtjfXVydX1qbQ==/?indoorLevel=1&ll=30.328975%2C59.934200&utm_medium=mapframe&utm_source=maps&z=17"
+            style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '14px' }}
+          >
+            Думская улица, 1-3 на карте Санкт‑Петербурга, ближайшее метро Невский проспект — Яндекс Карты
+          </a>
+          <iframe
+            title="Map"
+            // eslint-disable-next-line max-len
+            src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=30.328975%2C59.934200&mode=whatshere&whatshere%5Bpoint%5D=30.328975%2C59.934199&whatshere%5Bzoom%5D=17&z=17"
+            width="100%"
+            height="100%"
+            frameBorder="1"
+            allowFullScreen
+            style={{ position: 'relative' }}
+          />
+        </div>
+      </div>
     </section>
   );
 }
