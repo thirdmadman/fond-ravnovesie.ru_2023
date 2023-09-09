bb5b554f483c4b8b2870f3891f91f66867bb0272
thirdmadman <thirdmadman@gmail.com>
2023-09-09 11:24:24 +0200

    feat: add report card

---
+++ src/components/reports/ReportCard.tsx
@@ -0,0 +1,19 @@
+import '../../styles/reports/reportCard.scss';
+
+interface ReportCardProps {
+  year: number;
+  description: string;
+  link: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function ReportCard({ year, description, link }: ReportCardProps) {
+  return (
+    <div className="report-card">
+      <h3>{year}</h3>
+      <a href={link}>
+        <p>Смотреть</p>
+      </a>
+    </div>
+  );
+}
