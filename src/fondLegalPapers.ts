1c566b1d47a74097d3ec0eca294d44bd2013e508
thirdmadman <thirdmadman@gmail.com>
2023-09-11 21:07:45 +0200

    feat: add fondLegalPapers

---
+++ src/fondLegalPapers.ts
@@ -0,0 +1,24 @@
+export const fondLegalPapers = {
+  data: [
+    {
+      src: '/images/legal-papers/image144.png',
+      description: 'Устав фонда',
+      link: '',
+    },
+    {
+      src: '/images/legal-papers/image141.png',
+      description: 'Выписка из ЕГРЮЛ',
+      link: '',
+    },
+    {
+      src: '/images/legal-papers/image143.png',
+      description: 'Свидетельство о регистрации НКО',
+      link: '',
+    },
+    {
+      src: '/images/legal-papers/image142.png',
+      description: 'Свидетельство ФНС',
+      link: '',
+    },
+  ],
+};
