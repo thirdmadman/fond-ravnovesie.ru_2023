570c588c047c98c958e76700acdfa31f2b936641
thirdmadman <thirdmadman@gmail.com>
2023-11-04 21:11:38 +0100

    feat: correct code composition

--- src/components/home/SectionPrograms.tsx
+++ src/components/home/SectionPrograms.tsx
@@ -1,6 +1,16 @@
 import '../../styles/home/sectionPrograms.scss';
 import { fondProjects } from '@/fondProjects';
+import Link from 'next/link';
 import { ProjectGroupCard } from './ProjectGroupCard';
+
+const getHelpSteps = [
+  'Перейдите в нашу группу Вконтакте и напишите в сообщения группы, что хотите получить помощь',
+  'Бот в сообщениях предложит на выбор несколько проектов',
+  'После выбора проекта бот предложит заполнить анкету',
+  'После отправления анкеты, с вами свяжется координатор и объяснит дальнейшее взаимодействие',
+];
+
+const actionsButtons = [{ name: 'ПЕРЕЙТИ В ГРУППУ ВКОНТАКТЕ', link: '' }];
 
 export function SectionPrograms() {
   const getProjectGroupCards = () => {
@@ -28,14 +38,18 @@ export function SectionPrograms() {
       <div className="section-programs__get-help get-help">
         <div className="get-help__title">КАК ПОЛУЧИТЬ ПОМОЩЬ</div>
         <ol className="get-help__list">
-          <li>Перейдите в нашу группу Вконтакте и напишите в сообщения группы, что хотите получить помощь</li>
-          <li>Бот в сообщениях предложит на выбор несколько проектов</li>
-          <li>После выбора проекта бот предложит заполнить анкету</li>
-          <li>После отправления анкеты, с вами свяжется координатор и объяснит дальнейшее взаимодействие</li>
+          {getHelpSteps.map((el, i) => (
+            <li key={el}>
+              <span>{i + 1}</span>
+              <span>{el}</span>
+            </li>
+          ))}
         </ol>
-        <button className="get-help__button-to-vk" type="button">
-          ПЕРЕЙТИ В ГРУППУ ВКОНТАКТЕ
-        </button>
+        {actionsButtons.map((el) => (
+          <Link className="get-help__button-to-vk" href={el.link} key={el.link}>
+            {el.name}
+          </Link>
+        ))}
       </div>
       <div className="section-programs__fond-projects fond-projects">
         <div className="fond-projects__title">ПРОЕКТЫ ФОНДА</div>
