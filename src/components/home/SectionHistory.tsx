0ed52c46301e5da82fd59e7ba0143afb138f1ad1
thirdmadman <thirdmadman@gmail.com>
2023-11-05 15:21:22 +0100

    feat: add adaptivity to mobile-xl

--- src/components/home/SectionHistory.tsx
+++ src/components/home/SectionHistory.tsx
@@ -4,6 +4,7 @@ export function SectionHistory() {
   return (
     <section className="section-history">
       <div className="section-history__title">На своем опыте знаю, как сильно мешает психическое расстройство ...</div>
+      <div className="section-history__title-mobile">ИСТОРИЯ УЧРЕДИТЕЛЯ</div>
       <div className="section-history__text">
         <div className="section-history__column">
           <h4 className="section-history__header">ДЕТСТВО</h4>
