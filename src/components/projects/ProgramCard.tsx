bdb3ddb59b19e13135c96bae5459c4c7d960ca65
thirdmadman <thirdmadman@gmail.com>
2024-02-06 02:32:49 +0100

    feat: impl accordion button function

--- src/components/projects/ProgramCard.tsx
+++ src/components/projects/ProgramCard.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import '@/styles/projects/programCard.scss';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface IProgramCardProps {
   number: number;
@@ -8,18 +11,36 @@ interface IProgramCardProps {
 }
 
 export function ProgramCard({ number, name, description }: IProgramCardProps) {
+  const [isDescriptionHidden, setIsDescriptionHidden] = useState(true);
+
   return (
     <div className="program-card">
-      <div className="program-card__number">
-        <p>{`0${number}`}</p>
+      <div className="program-card__header">
+        <div className="program-card__number">
+          <p>{`0${number}`}</p>
+        </div>
+        <div className="program-card__body">
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+        <button
+          className="program-card__button"
+          aria-label="Read more"
+          type="button"
+          onClick={() => setIsDescriptionHidden(!isDescriptionHidden)}
+        >
+          <Image
+            src="images/icons/icon-arrow-down-black-rounded-accordion.svg"
+            width={50}
+            height={26}
+            alt="arrow down"
+            style={{ rotate: isDescriptionHidden ? '0deg' : '180deg' }}
+          />
+        </button>
       </div>
-      <div className="program-card__body">
-        <h3>{name}</h3>
-        <p>{description}</p>
+      <div className={`program-card__description ${isDescriptionHidden && 'program-card__description_is-hidden'}`}>
+        <p>Some text here</p>
       </div>
-      <button className="program-card__button" aria-label="Read more" type="button">
-        <Image src="images/icons/icon-arrow-down-black-rounded-accordion.svg" width={50} height={26} alt="arrow down" />
-      </button>
     </div>
   );
 }
