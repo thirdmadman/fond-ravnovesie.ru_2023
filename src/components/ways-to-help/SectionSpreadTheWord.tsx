c5a8b3d545b4b830586403d99c7339796da1a21a
thirdmadman <thirdmadman@gmail.com>
2024-02-18 01:54:09 +0100

    feat: add section spread the word text

--- src/components/ways-to-help/SectionSpreadTheWord.tsx
+++ src/components/ways-to-help/SectionSpreadTheWord.tsx
@@ -1,9 +1,35 @@
+import '@/styles/ways-to-help/sectionSpreadTheWord.scss';
+
+const dummyDescription =
+  'блок текста на 300 символов на символов блок текста блок текста на 300 символов на символов блок текста блок текста на 300 символов на символов блок текста блок текста на 300 символов на символов блок текста блок текста на 300 символов на символов блок текста блок текста на 300 символов на символов блок текста ';
+
+const spreadTheWordWays = [
+  {
+    title: 'Распространение информации о Фонде',
+    description: dummyDescription,
+  },
+  { title: 'Для блогеров и пользователей соц сетей', description: dummyDescription },
+  { title: 'Для журналистов', description: dummyDescription },
+  { title: 'Для компаний', description: dummyDescription },
+  { title: 'Для контента-мейкера, рекламных и креативных агентств', description: dummyDescription },
+];
+
 export function SectionSpreadTheWord() {
   return (
     <section className="section-spread-the-word">
       <h2>Распространить информацию</h2>
-      <p>Следите за нами в социальных сетях и делитесь нашими достижениями.</p>
-      <p>Поделитесь своими знакомствами с другими людьми и поможете нам расширить нашу аудиторию.</p>
+      <div className="section-spread-the-word__main-text">
+        {spreadTheWordWays.map((way) => (
+          <div className="section-spread-the-word__text-line text-line" key={way.title}>
+            <div className="text-line__title">
+              <p>{way.title}</p>
+            </div>
+            <div className="text-line__description">
+              <p>{way.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
