615ae1e34e54435b775e5b3299918748f87f7f63
thirdmadman <thirdmadman@gmail.com>
2024-02-18 17:31:54 +0100

    feat: update SectionProvidePremises

--- src/app/ways-to-help/page.tsx
+++ src/app/ways-to-help/page.tsx
@@ -1,12 +1,12 @@
 import { Header } from '@/components/shared/Header';
 import { Footer } from '@/components/shared/Footer';
 import { Metadata } from 'next';
-import '@/styles/ways-to-help/ways-to-help.scss';
 import { SectionWaysDonation } from '@/components/ways-to-help/SectionWaysDonation';
 import { SectionBecomePartner } from '@/components/ways-to-help/SectionBecomePartner';
 import { SectionBecomeVolunteer } from '@/components/ways-to-help/SectionBecomeVolunteer';
 import { SectionSpreadTheWord } from '@/components/ways-to-help/SectionSpreadTheWord';
 import { SectionProvidePremises } from '@/components/ways-to-help/SectionProvidePremises';
+import '@/styles/ways-to-help/ways-to-help.scss';
 
 export const metadata: Metadata = {
   title: 'Помочь фонду | Благотворительный фонд Равновесие',
--- src/components/ways-to-help/SectionProvidePremises.tsx
+++ src/components/ways-to-help/SectionProvidePremises.tsx
@@ -1,13 +1,37 @@
+import Image from 'next/image';
+
+import '@/styles/ways-to-help/sectionProvidePremises.scss';
+
 export function SectionProvidePremises() {
   return (
     <section className="section-provide-premises">
       <h2>Предоставить помещение</h2>
-      <p>Для чего нам нужны помещения</p>
-      <p>
-        Тренинги — Группы поддержки — Терапевтические группы — Арт-группы — Индивидуальные консультации — Встречи с
-        волонтерами — Интервизии и супервизии — Встречи равных консультантов с подопечными — Лекции — Открытые
-        мероприятия, праздники
-      </p>
+      <div className="section-provide-premises__body">
+        <div className="section-provide-premises__image-container">
+          <div className="section-provide-premises__image-wrapper">
+            <Image
+              src="/images/ways-to-help/provide-premises-image.png"
+              alt="photo of use example"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+        <div className="section-provide-premises__text">
+          <h3>Для чего нам нужны помещения</h3>
+          <p className="section-provide-premises__purposes">
+            Тренинги — Группы поддержки — Терапевтические группы — Арт-группы — Индивидуальные консультации — Встречи с
+            волонтерами — Интервизии и супервизии — Встречи равных консультантов с подопечными — Лекции — Открытые
+            мероприятия, праздники
+          </p>
+          <p className="section-provide-premises__description">
+            Почти для всех наших проектов нужны пространства.Мы будем рады помещению на постоянной основе, регулярной,
+            временной или одноразовой, так как у наших проектов разные задачи. А также размеры помещений могут быть
+            различными — например, зал для лекции на 100 человек, помещение для тренинга на 12 человек или кабинет для
+            консультации на двоих
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
