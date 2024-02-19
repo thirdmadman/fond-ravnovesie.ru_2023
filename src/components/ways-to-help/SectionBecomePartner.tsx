047d3a4c94420f640a66a267c9cad36bb00237a2
thirdmadman <thirdmadman@gmail.com>
2024-02-19 12:28:29 +0100

    feat: add wrapper

--- src/components/ways-to-help/SectionBecomePartner.tsx
+++ src/components/ways-to-help/SectionBecomePartner.tsx
@@ -3,14 +3,19 @@ import '@/styles/ways-to-help/sectionBecomePartner.scss';
 export function SectionBecomePartner() {
   return (
     <section className="section-become-partner" id="become-partner">
-      <h2>Стать партнёром</h2>
-      <h3>(ЮРИДИЧЕСКИМ ЛИЦАМ)</h3>
-      <p className="section-become-partner__description">
+      <div className="section-become-partner__heading">
+        <h2>Стать партнёром</h2>
+        <h4>(ЮРИДИЧЕСКИМ ЛИЦАМ)</h4>
+      </div>
+      <p className="section-become-partner__overall-description">
         блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов
         блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов
         блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов
         блок текста на 400 символов блок текста на 400 символов блок текста на 400 символов
       </p>
+      <div className="section-become-partner__financial-help">
+        <h3>Финансовая помощь</h3>
+      </div>
     </section>
   );
 }
