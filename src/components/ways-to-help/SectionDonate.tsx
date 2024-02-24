28cf96d08c4e1bbd5b73965ce312f9ba0225ec93
thirdmadman <thirdmadman@gmail.com>
2024-02-24 21:37:20 +0100

    feat: update SectionDonate

--- src/components/ways-to-help/SectionDonate.tsx
+++ src/components/ways-to-help/SectionDonate.tsx
@@ -1,11 +1,19 @@
+import { DonationForm } from '../home/DonationForm';
+import '@/styles/ways-to-help/sectionDonate.scss';
+
 export function SectionDonate() {
   return (
-    <section className="section-ways-donation" id="donate">
+    <section className="section-donate" id="donate">
       <h2>Пожертвовать</h2>
-      <p>
-        блок текста о том, на что мы собираем блок текста о том, на что мы собираем блок текста о том, на что мы
-        собираем блок текста о том, на что мы собираем блок текста о том, на что мы собираем
-      </p>
+      <div className="section-donate__body">
+        <DonationForm />
+        <div className="section-donate__description">
+          <p>
+            блок текста о том, на что мы собираем блок текста о том, на что мы собираем блок текста о том, на что мы
+            собираем блок текста о том, на что мы собираем блок текста о том, на что мы собираем
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
